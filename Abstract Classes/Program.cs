3ab1245f12fc53090615165a319efb683f0e2197
Trenton Isenor <ronesinotnert@gmail.com>
2021-11-10 09:23:09 -0400

    Initial commit

---
+++ Abstract Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a Abstract Classes Demo");
+        }
+    }
+}
